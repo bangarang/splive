d92469a9c46ff1911e7b75b5cc20ebf5565e4870
bangarang <me@alexjrock.com>
2013-09-03 18:33:46 -0500

    redistogo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Splive::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
