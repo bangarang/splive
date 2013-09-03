63b0e474cf4f4ac28b6fe34211ee8883b89cd39c
bangarang <me@alexjrock.com>
2013-09-03 18:29:15 -0500

    redistogo

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,6 @@
-$redis = Redis.new
+if Rails.env.development?
+	$redis = Redis.new
+elsif Rails.env.production?
+	uri = URI.parse(ENV["REDISTOGO_URL"])
+	REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
