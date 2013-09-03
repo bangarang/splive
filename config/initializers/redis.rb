497cfd0799aab0615af3d75195cbd31bfbf98cf7
bangarang <me@alexjrock.com>
2013-09-03 18:50:11 -0500

    redistogo

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,5 +3,5 @@
 # elsif Rails.env.production?
 	# ENV["REDISTOGO_URL"] ||= 'redis://localhost:6379'
 	uri = URI.parse(ENV["REDISTOGO_URL"])
-	$redis = REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+	$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 # end
