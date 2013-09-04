f1a2a2c22e0ca47bf5ac0ea8fa95007fbd2a1a70
bangarang <me@alexjrock.com>
2013-09-03 19:41:36 -0500

    redistogo

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,4 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# use provided URL or default to localhost
+redis_uri = URI.parse(ENV['REDISTOGO_URL'] || 'redis://127.0.0.1')
+# set up a connection to Redis for manual caching
+$redis = Redis.new(:host => redis_uri.host, :port => redis_uri.port, :password => redis_uri.password)
