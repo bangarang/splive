d39d2514cc187c6f53909f2dd5277e839d952096
bangarang <me@alexjrock.com>
2013-09-03 19:10:27 -0500

    redistogo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Splive::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  $redis = Redis.new
+  ENV["REDISTOGO_URL"] = 'redis://localhost:6379'
 end
