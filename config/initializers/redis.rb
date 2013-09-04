06b2f2b3f12840d463190180713b1cdb5046e081
bangarang <me@alexjrock.com>
2013-09-03 20:27:48 -0500

    redistogo

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,8 +2,7 @@ if Rails.env.development?
 	$redis = Redis.new
 elsif Rails.env.production?
 	# use provided URL or default to localhost
-	redis_uri = URI.parse(ENV['REDISTOGO_URL']
+	redis_uri = URI.parse(ENV['REDISTOGO_URL'])
 	# set up a connection to Redis for manual caching
-	REDIS = Redis.new(:host => redis_uri.host, :port => redis_uri.port, :password => redis_uri.password)
-	$redis = REDIS
+	$redis = REDIS = Redis.new(:host => redis_uri.host, :port => redis_uri.port, :password => redis_uri.password)
 end
